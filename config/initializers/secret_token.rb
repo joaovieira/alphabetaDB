e454d544e480b2c929013fa446e88c26d3cc1bd8
Joao Vieira <shocker@abandonados.info>
2011-10-05 17:27:00 +0200

    App skeleton and db connection set up properly. Index default. Ready to start.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlphabetaDB::Application.config.secret_token = 'c11f12e7547a6904024b4f51eca77d078fccdadf73aedbdaaef261c07785fa417c4510de8dbf82c22089b8a5fd455ab57218460d74838666334876631aff3b73'
